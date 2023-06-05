d3a90c6809fcb8e91d2f10c3c6db3d1ef5ec406c
Slmn <sulaman.khan@vaivaltech.com>
2023-06-05 18:21:55 +0500

    New changes.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
                     sidebarPath: require.resolve('./sidebars.js'),
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl: 'https://github.com/BloxBytes/elysium-docs/',
+                    editUrl: 'https://github.com/BloxBytes/elysium-docs/tree/master/',
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
