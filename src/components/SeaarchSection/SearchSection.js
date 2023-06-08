76af6060723196e6f5b759ab5214a96b430055ef
Slmn <sulaman.khan@vaivaltech.com>
2023-06-08 14:37:08 +0500

    new changes of docker.

--- src/components/SeaarchSection/SearchSection.js
+++ src/components/SeaarchSection/SearchSection.js
@@ -2,24 +2,38 @@ import React from 'react'
 import Logo from '../../../static/images/logo.png'
 import SearchIcon from '../../../static/images/Search.png'
 import {ReactComponent as Eventicon} from '../../../static/images/icon-events.svg'
-import {InputGroup,InputGroupText,Input} from 'reactstrap'
-export default function SearchSection(){
+import {InputGroup, InputGroupText, Input} from 'reactstrap'
+// import TypesenseInstantsearchAdapter from "typesense-instantsearch-adapter";
+// import {InstantSearch} from "react-instantsearch-dom";
 
-    return(
+
+// const typesenseInstantSearchAdapter = new TypesenseInstantsearchAdapter({
+//     server: {
+//         apiKey: '',
+//         nodes: [],
+//     }
+// });
+export default function SearchSection() {
+
+    return (
         <>
             <section className={'searchSectoin clients'}>
                 <div className="container">
                     <div className="row">
                         <div className="col-5 mx-auto">
-                            <h2 className={'RadialGradient top-minus text-capitalize text-white text-center Fsize_48 ArchivoLight pb-3 text-shadow'}>Elysium <span className={"ArchivoBold"}>Documentation</span></h2>
-                            <p className={'pb-3'}>Elysium documentation includes conceptual, procedural, and reference information for blockchain builders and parachain project teams</p>
+                            <h2 className={'RadialGradient top-minus text-capitalize text-white text-center Fsize_48 ArchivoLight pb-3 text-shadow'}>Elysium <span
+                                className={"ArchivoBold"}>Documentation</span></h2>
+                            <p className={'pb-3'}>Elysium documentation includes conceptual, procedural, and reference
+                                information for blockchain builders and parachain project teams</p>
                         </div>
                         <div className="col-8 mx-auto">
                             <InputGroup>
                                 <InputGroupText>
-                                    <img src={SearchIcon} />
+                                    <img src={SearchIcon}/>
                                 </InputGroupText>
-                                <Input placeholder="and..." />
+                                {/*<InstantSearch></InstantSearch>*/}
+
+                                <Input placeholder="and..."/>
                                 <button className={'btn BtnGreen'}>Search</button>
                             </InputGroup>
                         </div>
